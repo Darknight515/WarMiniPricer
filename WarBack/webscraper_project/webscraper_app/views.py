3e5ad6abf4fd0bc9e037d58b29154da449f6734c
Jorge Quinones <jorgeqdark.17@gmail.com>
2024-02-18 15:03:38 -0400

    changed scrape source targeting non dynamic rendered page

--- WarBack/webscraper_project/webscraper_app/views.py
+++ WarBack/webscraper_project/webscraper_app/views.py
@@ -1,20 +1,37 @@
 from django.shortcuts import render
 from bs4 import BeautifulSoup
 from django.http import JsonResponse
+
 import requests
 # Create your views here.
 
+
+
 def scrape_website(url):
     response = requests.get(url)
-    soup = BeautifulSoup(response.text,'html.parser')
-    spans = soup.find_all('div')
-    text_content = []
-    for span in spans:
-        text_content.append(span.get_text())
-    return text_content
+    print("URL Response Status:", response.status_code)
 
+    soup = BeautifulSoup(response.text, 'html.parser')
+    
+    # Extracting all product details divs
+    product_details_divs = soup.find_all('div', class_='product-details')
+    print("Product Details Elements found: ", bool(product_details_divs))
+    
+    data = []
+    for product in product_details_divs:
+        # Extract product name
+        product_name_tag = product.find('span', class_='title')
+        product_name = product_name_tag.get_text().strip() if product_name_tag else "Name not found"
+        
+        # Extract current price
+        current_price_tag = product.find('span', class_='current_price')
+        current_price = current_price_tag.find('span', class_='money').get_text().strip() if current_price_tag else "Price not found"
+        
+        data.append({'product_name': product_name, 'current_price': current_price})
+    
+    return data
 
 def scrape_view(request):
-    url = 'https://www.warhammer.com/en-US/shop/warhammer-40000/armies-of-the-imperium/astra-militarum'
+    url = 'https://shoparmada.com/collections/gw40k-astra-militarum'
     data = scrape_website(url)
-    return JsonResponse({'headings': data})
+    return JsonResponse({'products': data})
