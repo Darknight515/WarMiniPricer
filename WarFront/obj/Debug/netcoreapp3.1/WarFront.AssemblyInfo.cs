e3b32c5eba70b03b02b322d3b4a375f3f9b4b427
Brandon J. Baker <brandon.j.baker1011@gmail.com>
2024-04-28 20:45:40 -0400

    scrapy class files

--- WarFront/obj/Debug/netcoreapp3.1/WarFront.AssemblyInfo.cs
+++ WarFront/obj/Debug/netcoreapp3.1/WarFront.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WarFront")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+650e7d347da17652602074eed551f456acfd75e9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f6752c4bad1d2f9fbea5d622ad0ae993ce48795")]
 [assembly: System.Reflection.AssemblyProductAttribute("WarFront")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WarFront")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
