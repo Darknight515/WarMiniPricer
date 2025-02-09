79a4fc0e67dfd0d1e42cf577bb9a2d61ba001357
Brandon Baker <brandon.j.baker1011@gmail.com>
2025-02-09 01:20:39 -0400

    minor styling & adjustments.

--- WarFront/war-front/src/components/DisplayCard.jsx
+++ WarFront/war-front/src/components/DisplayCard.jsx
@@ -1,13 +1,23 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import { useMiniContext } from "../contexts/MiniContext";
 
-function DisplayCard({mini}){
-    const {isInCart, addMiniToCart, removeMiniToCart} = useMiniContext;
-    return(
-        <div className="rounded-md">
-            <div className="card-image"></div>
-            <div className="card-info">{mini.name}</div>
-            <div className="card-pricing"></div>
+function DisplayCard({ mini }) {
+    const { isInCart, addMiniToCart, removeMiniToCart } = useMiniContext;
+    return (
+        <div className="
+        rounded-md
+        bg-slate-400
+        max-w-60
+        flex
+        flex-col
+        items-center
+        justify-center
+        p-4">
+            <div className="card-image flex justify-center max-w-auto">
+                <img src={mini.image_url} alt="mini image" />
+            </div>
+            <div className="card-info text-center mt-2">{mini.name}</div>
+            <div className="card-pricing">{mini.price}</div>
         </div>
     )
 }
